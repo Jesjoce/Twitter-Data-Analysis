d02dbf973796d3193497e5d9b6797aa3b4b3ca7d
Jesjoce <mukamisha.jocelyne@gmail.com>
2021-06-23 20:35:59 +0200

    changes to the clean_tweets

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import datetime
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -13,28 +13,31 @@ class Clean_Tweets:
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
+        list_col = list(df.columns)
+
+        for col in list_col:
+            try:
+                unwanted_rows = df[df[col] == col ].index
+            except:
+                continue
+            df.drop(unwanted_rows , inplace=True)
         
         return df
+
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
         
-        ---
+        df.drop_duplicates(subset=['created_at', 'source', 'original_text', 'clean_text', 'polarity','subjectivity','lang',
+        'favorite_count', 'retweet_count','original_author', 'followers_count','friends_count','place'], keep = 'first', inplace=True)
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
-        
-        df = df[df['created_at'] >= '2020-12-31' ]
+        df['created_at'] = pd.to_datetime(df['created_at'], format='%a %b %d %H:%M:%S +0000 %Y')
         
         return df
     
@@ -43,18 +46,15 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+        "They are number"
         
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove non english tweets from lang
-        """
-        
-        df = ----
-        
+        """   
+        Non_english_tweets = df[df['lang']!='en']
+        df.drop(Non_english_tweets , inplace=True)
+
         return df
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -39,6 +39,13 @@ class TweetDfExtractor:
             text.append(tweets['text'])
         return text
     
+    def find_clean_text(self, text)->list:
+        clean_text = []
+        for t in text:
+            new_t = re.sub(r'^.*?:', '', t)
+            clean_text.append(new_t)
+        return clean_text
+    
     def find_screen_name(self)->list:
         screen_name = []
         for tweets in self.tweets_list:
@@ -95,7 +102,11 @@ class TweetDfExtractor:
     def find_retweet_count(self)->list:
         retweet_count = []
         for tweets in self.tweets_list:
-            retweet_count.append(tweets['retweet_count'])
+            try: 
+                retweet = tweets['retweeted_status']['retweet_count']
+            except:
+                retweet = None
+            retweet_count.append(retweet)
         return retweet_count
 
     def find_hashtags(self)->list:
@@ -125,12 +136,13 @@ class TweetDfExtractor:
     def get_tweet_df(self, save=False)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
         
-        columns = ['created_at','source', 'original_text', 'polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 'original_author',
+        columns = ['created_at','source', 'original_text','clean_text', 'polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 'original_author',
              'followers_count','friends_count', 'hashtags', 'user_mentions','place']
         
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
+        clean_text = self.find_clean_text(text)
         polarity, subjectivity = self.find_sentiments(text)
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
@@ -142,7 +154,7 @@ class TweetDfExtractor:
         mentions = self.find_mentions()
         location = self.find_location()
         
-        data = list(zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, hashtags, mentions, location))
+        data = list(zip(created_at, source, text, clean_text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, hashtags, mentions, location))
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
@@ -154,9 +166,9 @@ class TweetDfExtractor:
                 
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-    'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    columns = ['created_at','source', 'original_text', 'clean_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 'original_author',
+             'followers_count','friends_count', 'hashtags', 'user_mentions','place']
+    _, tweet_list = read_json("./data/covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
