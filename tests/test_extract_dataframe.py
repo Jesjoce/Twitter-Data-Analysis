d6ec35c176d92f98b86a271b6c4032ddceab4253
Jesjoce <mukamisha.jocelyne@gmail.com>
2021-06-28 11:57:51 +0200

    corrected_test_hashtags_results

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from extract_dataframe import read_json
 from extract_dataframe import TweetDfExtractor
 
-_, tweet_list = read_json("./data/covid19.json")
+_, tweet_list = read_json("../data/covid19.json")
 
 columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
@@ -74,7 +74,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(self.df.find_retweet_count(), [612, 92, 1, 899, 20])
 
     def test_find_hashtags(self):
-         self.assertEqual(self.df.find_hashtags(),[[],[],'red4research',[],[]] )
+         self.assertEqual(self.df.find_hashtags(),[[],[],['red4research'],[],[]] )
 
     def test_find_mentions(self):
          self.assertEqual(self.df.find_mentions(), [['TelGlobalHealth', 'WHOAFRO'], ['globalhlthtwit'], ['NHSRDForum', 'Research2note', 'NHSRDForum'],['HighWireTalk'],['PeterHotez']])
